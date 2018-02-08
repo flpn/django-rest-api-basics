acff1c09da697661d9e15251cb07d6012d7c98bb
Felipe Netto <flpncc@gmail.com>
2018-02-08 10:02:32 -0300

    API create and list views

--- postings/api/urls.py
+++ postings/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import BlogPostRUDView
+from . import views
 
 
 app_name = 'postings'
 
 urlpatterns = [
-    path('<int:pk>/', BlogPostRUDView.as_view(), name='post-rud'),
+    path('', views.BlogPostAPIView.as_view(), name='post-list-create'),
+    path('<int:pk>/', views.BlogPostRudAPIView.as_view(), name='post-rud'),
 ]
--- postings/api/views.py
+++ postings/api/views.py
@@ -1,10 +1,25 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 
 from postings.models import BlogPost
 from .serializers import BlogPostSerializer
 
 
-class BlogPostRUDView(generics.RetrieveUpdateDestroyAPIView):
+class BlogPostAPIView(mixins.CreateModelMixin ,generics.ListAPIView):
+    lookup_field = 'pk'
+    serializer_class = BlogPostSerializer
+
+    def get_queryset(self):
+        return BlogPost.objects.all()
+
+    # Handled by CreateModelMixin
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class BlogPostRudAPIView(generics.RetrieveUpdateDestroyAPIView):
     lookup_field = 'pk'
     serializer_class = BlogPostSerializer
 
